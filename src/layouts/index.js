de17c36661aebd4037dc01fbf59416bd6415baeb
torokokill <torokokill@gmail.com>
2018-03-30 08:25:57 -0700

    First commit

---
+++ src/layouts/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+class Template extends React.Component {
+  render() {
+    const { location, children } = this.props;
+    let header;
+    if (location.pathname === '/') {
+      header = (
+        <h1>
+          <Link to="/">Gatsby Starter Blogaqwqwqfwassda</Link>
+        </h1>
+      );
+    } else {
+      header = (
+        <h3>
+          <Link to="/">Gatsby Starter Blogabqwdwqd</Link>
+        </h3>
+      );
+    }
+    return (
+      <div>
+        {header}
+        {children()}
+      </div>
+    );
+  }
+}
+
+Template.propTypes = {
+  children: React.PropTypes.func,
+  location: React.PropTypes.object,
+  route: React.PropTypes.object,
+};
+
+export default Template;
---
+++ src/pages/2018-03-11-hello-world/index.md
@@ -0,0 +1,7 @@
+---
+title: Hello World
+date: "2018-03-11T13:52:03.284Z"
+path: "/hello-world/"
+---
+
+In my infinite wisdom, I've decided to redo my site, transitioning from pelican to gatsby. Bare with me while this gets rebuilt from scratch.
