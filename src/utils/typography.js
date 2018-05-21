4d4026ee397320b30f83124aaa0e56a57a3465b3
Patrick Owens <1919554+powens@users.noreply.github.com>
2018-05-20 21:18:27 -0700

    Fork typography theme

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,5 +1,79 @@
 import Typography from 'typography';
-import theme from 'typography-theme-judah';
+// import theme from 'typography-theme-judah';
+
+import gray from 'gray-percentage';
+import { MOBILE_MEDIA_QUERY } from 'typography-breakpoint-constants';
+import colors from './colors';
+
+const theme = {
+  title: 'Boop',
+  baseFontSize: '18px',
+  baseLineHeight: 5 / 3,
+  googleFonts: [
+    {
+      name: 'Roboto Condensed',
+      styles: ['400', '400i'],
+    },
+    {
+      name: 'Vollkorn',
+      styles: ['400', '400i', '700'],
+    },
+  ],
+  headerFontFamily: ['Roboto Condensed', 'sans-serif'],
+  bodyFontFamily: ['Vollkorn', 'Georgia', 'serif'],
+  headerColor: 'hsla(0,0%,0%,1)',
+  bodyColor: 'hsla(0,0%,0%,0.8)',
+  headerWeight: 400,
+  bodyWeight: 400,
+  boldWeight: 700,
+  overrideStyles: ({ adjustFontSizeTo, scale, rhythm }, options) => ({
+    a: {
+      color: colors.primary,
+      textDecoration: 'none',
+    },
+    'a:hover,a:active': {
+      color: options.bodyColor,
+      textDecoration: 'none',
+    },
+    blockquote: {
+      ...scale(1 / 5),
+      color: gray(0),
+      fontStyle: 'italic',
+      paddingLeft: rhythm(13 / 16),
+      marginLeft: 0,
+      borderLeft: `${rhythm(3 / 16)} solid ${gray(80)}`,
+    },
+    'blockquote > :last-child': {
+      marginBottom: 0,
+    },
+    'blockquote cite': {
+      ...adjustFontSizeTo(options.baseFontSize),
+      color: options.bodyColor,
+      fontWeight: options.bodyWeight,
+    },
+    'blockquote cite:before': {
+      content: '"— "',
+    },
+    ul: {
+      listStyle: 'disc',
+    },
+    [MOBILE_MEDIA_QUERY]: {
+      'ul,ol': {
+        marginLeft: rhythm(1),
+      },
+      blockquote: {
+        marginLeft: rhythm(-3 / 4),
+        marginRight: 0,
+        paddingLeft: rhythm(9 / 16),
+      },
+    },
+    'h5,h6': {
+      textTransform: 'uppercase',
+      fontStyle: 'italic',
+    },
+  }),
+};
+
 
 const typography = new Typography(theme);
 
