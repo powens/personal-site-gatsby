e8d3730637ad793dbf5877afb5cb865c4caca470
Patrick Owens <1919554+powens@users.noreply.github.com>
2018-06-04 10:42:12 -0700

    Added pre-load of styles

--- src/components/SocialBlock.js
+++ src/components/SocialBlock.js
@@ -20,14 +20,28 @@ const IconWrapper = styled.div`
 const SocialLink = styled.a`
   font-size: 1.75rem;
   margin-left: 0.2rem;
+`;
+
+const PreloadedImage = css`
+  display: inline-block;
+  font-size: inherit;
   height: 1em;
+  vertical-align: -.125em;
+  width: 0.875rem;
+  overflow: visible;
 `;
 
 const SocialBlock = () => (
   <IconWrapper>
-    <SocialLink href="https://github.com/powens" aria-label="Link to Github"><FontAwesomeIcon icon={faGithub} /></SocialLink>
-    <SocialLink href="https://twitter.com/torokokill" aria-label="Link to Twitter"><FontAwesomeIcon icon={faTwitter} /></SocialLink>
-    <SocialLink href="https://mastodon.social/powens" aria-label="Link to Mastodon"><FontAwesomeIcon icon={faMastodon} /></SocialLink>
+    <SocialLink href="https://github.com/powens" aria-label="Link to Github">
+      <FontAwesomeIcon icon={faGithub} className={PreloadedImage} />
+    </SocialLink>
+    <SocialLink href="https://twitter.com/torokokill" aria-label="Link to Twitter">
+      <FontAwesomeIcon icon={faTwitter} className={PreloadedImage} />
+    </SocialLink>
+    <SocialLink href="https://mastodon.social/powens" aria-label="Link to Mastodon">
+      <FontAwesomeIcon icon={faMastodon} className={PreloadedImage} />
+    </SocialLink>
   </IconWrapper>
 );
 
