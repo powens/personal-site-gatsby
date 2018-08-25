15f0f1173aab2e358b79e1699c2b5194d9d3f531
Patrick Owens <1919554+powens@users.noreply.github.com>
2018-08-25 09:13:46 -0700

    Added HMR support for typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -79,7 +79,16 @@ const theme = {
 };
 
 
+
+
 const typography = new Typography(theme);
+
+// Hot reload typography in development.
+if (process.env.NODE_ENV !== 'production') {
+  typography.injectStyles()
+}
+
+
 const { rhythm, scale } = typography;
 
 export { rhythm, scale, typography as default };
