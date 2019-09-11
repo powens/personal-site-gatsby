af67a8673814ae0d8c0c381ff93467c42b0e2259
Patrick Owens <1919554+powens@users.noreply.github.com>
2019-09-11 07:52:49 -0700

    Removing smartypants

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,7 +45,6 @@ module.exports = {
           },
           'gatsby-remark-prismjs',
           'gatsby-remark-copy-linked-files',
-          'gatsby-remark-smartypants',
         ],
       },
     },
