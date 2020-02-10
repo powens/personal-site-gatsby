4749ddf5661995e7ec091b7bc90ab03586e6699e
Patrick Owens <1919554+powens@users.noreply.github.com>
2020-02-10 10:31:09 -0800

    Reduce font size

--- src/gatsby-plugin-theme-ui/typography.ts
+++ src/gatsby-plugin-theme-ui/typography.ts
@@ -3,4 +3,6 @@ import 'typeface-work-sans';
 import theme from 'typography-theme-fairy-gates';
 import { toTheme } from '@theme-ui/typography';
 
+theme.baseFontSize = '16px';
+
 export default toTheme(theme);
